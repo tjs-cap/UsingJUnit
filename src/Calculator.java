55013ac0e26ba4401630c255a04214e9c6d2210c
Trevor Smith <104841977+tjs-cap@users.noreply.github.com>
2022-06-14 11:30:00 +0100

    CalulatorTest.Java Added new assert test.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int division(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        int total = add(2,2);
+        System.out.println(total);
+    }
 }
--- src/Tests/CalculatorTest.java
+++ src/Tests/CalculatorTest.java
@@ -1,4 +1,29 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+    @Test
+    void TwoPlusTwoShouldEqualFour(){
+        var cal = new Calculator();
+        assertEquals(4,cal.add(2,2));
+    }
+
+    @Test
+    void TwoPlusThreeShouldEqualFive(){
+        var cal = new Calculator();
+        assertEquals(5,cal.add(2,3));
+    }
+
+    @Test
+    void FourDivideByTwoShouldEqualTwo(){
+        var cal = new Calculator();
+        assertEquals(2,cal.division(4,2));
+    }
+
+    @Test
+    void EightDevideByTwoShouldEqualFour(){
+        var cal = new Calculator();
+        assertEquals(4,cal.division(8,2));
+    }
 }
